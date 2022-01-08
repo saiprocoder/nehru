5d49020cf1ac6cd86b1d6ab1311639281783f174
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2022-01-08 13:05:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var a=prompt ("Welcome to Jawaharlal Nehru Project! If you want to see our Jawaharlal Nehru Slides, Type Yes. If no, Type No.");
+
+    if (a=="Yes"){
+      window.location.href="https://bit.ly/jnslides"
+    }
+
+    if (a=="No"){
+      window.location.href="content.html"
+    }
